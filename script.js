b058b4e9808e2f722a2caf43a77dee6f741e501b
aburahadbinnishan007-dev <aburahadbinnishan007@gmail.com>
2026-08-04 04:57:07 +0600

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,70 @@
+const texts = [
+    "CSE Student",
+    "Aspiring Software Developer",
+    "AI Enthusiast",
+    "Python Developer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+
+        setTimeout(() => {
+
+            index = 0;
+            count++;
+
+            type();
+
+        },1500);
+
+    }else{
+
+        setTimeout(type,100);
+
+    }
+
+})();
+// Scroll To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+};
+
+topBtn.onclick = function () {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+};
