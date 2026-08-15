5230ab124061baf5a478313e5e4a4665753de4ed
aburahadbinnishan007-dev <aburahadbinnishan007@gmail.com>
2026-08-15 09:42:07 +0600

    Add mobile hamburger menu

--- script.js
+++ script.js
@@ -68,3 +68,9 @@ topBtn.onclick = function () {
     });
 
 };
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+});
